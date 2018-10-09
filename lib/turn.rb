e5ed5db948972f17f976247db32ffebb4e2e7aad
Karl Benbeneck <kbenbeneck@gmail.com>
2018-10-09 01:04:47 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,26 +10,16 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def valid_move?(array, index)
-  if (index < 0 || index > 8)
-    return false
-  elsif position_taken?(array, index)
-    return false
-  else
-    return true
-  end
-end
-
-def move(board, index, current_player)
+def move(board, index, current_player = "X")
   board[index] = current_player
 end
 
-def position_taken?(array, index)
-  if (array[index] == " " || array[index] == "" || array[index] == nil)
-    return false
-  else
-    return true
-  end
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
@@ -37,29 +27,9 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, current_player(board))
+    move(board, index)
     display_board(board)
   else
     turn(board)
-  end
-end
-
-def turn_count(array)
-  counter = 0
-  array.each do |element|
-    if (element == "X" || element == "O")
-      counter += 1
-    else
-      # do nothing
-    end
-  end
-  return counter
-end
-
-def current_player(board)
-  if (turn_count(board).even? == true)
-    return "X"
-  else
-    return "O"
   end
 end
