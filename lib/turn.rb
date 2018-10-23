cda041ff4b1a84c338ce5cb1ca42e310088c3b57
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-10-23 22:25:05 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,3 +7,20 @@ def position_taken?(board,index)
   !(board[index] == " " || board[index] == "" || board[index] == nil) ?
   true : false
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+index = user_input.to_i - 1          #convert to integer and covert to index
+return index                         #return index for use in other methods
+end
+
+def move (board,index,value = "X")
+board[index] = value                   #set the value of the board's index to X
+end
