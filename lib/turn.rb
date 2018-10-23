ebdfdc6239571b481e219153a3b4d04375f78f90
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-10-23 23:19:12 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,3 +24,16 @@ end
 def move (board,index,value = "X")
 board[index] = value                   #set the value of the board's index to X
 end
+
+def turn(board)
+puts "Please Enter 1-9:"
+user_input = gets.strip 
+input_to_index(user_input)
+if valid_move?(board,index) || !position_taken?(board,index)
+  move(board,index,value = "X")         #move function call, set default value variable
+  display_board(board)                  #display changed board
+else
+  puts "Please enter a valid input"
+  turn(board)
+end
+end
