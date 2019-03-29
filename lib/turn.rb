e70abf2347dcb2995eac56a9728e88f1aa6ba7b9
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-29 09:07:36 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,38 @@
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input_valid = false
+  while input_valid == false
+    index = input_to_index(gets.chomp)
+     if valid_move?(board, index)
+       input_valid = true
+       move(board, index)
+       display_board(board)
+     else
+       puts "invalid"
+     end
+  end
+end
