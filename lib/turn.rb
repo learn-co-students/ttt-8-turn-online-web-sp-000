3015272b5dc4672833d403042427c9f616149171
Connor Cowan <ccowan003@comcast.net>
2019-05-16 02:37:24 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,23 +6,23 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input,)
-  index = user_input.to_i - 1       #this here converts a user_input to integer, subtracts 1, and returns -1 for strings without integers
+def input_to_index(user_input)
+   user_input.to_i - 1               #this here converts a user_input to integer, subtracts 1, and returns -1 for strings without integers
 end                                 #sets it to variable index
 
 def valid_move?(board, index)
   if index.between?(0,8) #if index is within available positions on board
-    if !(position_taken?(board,index)) #if our position is not taken (call to our helper method position_taken?)
-      true                       # return true for valid move
+    if !position_taken?(board,index) #if our position is not taken (call to our helper method position_taken?)
+      return true                       # return true for valid move
     end
   end
 end
 
 def turn(board)
 
-  puts "Enter a number 1-9"
-  input = gets.strip
-  input = input_to_index(input)
+  puts "Please enter 1-9:"
+  input = gets.strip                #create local variable 'input' that gets input from player
+  index = input_to_index(input)     #convert input to correct index (#input_to_index(input))
 
   if valid_move?(board, index)      #if move is valid (allowed)
     move(board, index)
@@ -33,17 +33,10 @@ def turn(board)
 end
 
 def position_taken?(board, index)
-  # We're accessing our array with board[index]
-  taken = nil #initializing variable to nil
-  if (board[index] == " " || board[index] == "" || board[index] == nil)
-    taken = false #updating variable to false, saying index is currently empty
-  else
-    taken = true  #updating variable to index being true or "full"
-  end
-end
 
   # We're accessing our array with board[index]
-  #board[index] != " "    #if index not empty, position is taken
+  board[index] != " "    #if index not empty, position is taken
+end
 
 def move(board, index, value = "X")
   board[index] = value
