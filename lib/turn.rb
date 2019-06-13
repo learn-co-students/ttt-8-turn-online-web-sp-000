c3683fd633bcfcefefa48f3caf51b8d2ced9c064
Peter Grant <jgrantps@hotmail.com>
2019-06-13 12:17:37 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,3 @@
-
-
-#puts "Welcome to Tic Tac Toe!"
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -12,6 +8,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  #captures the user input.
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index) == true
@@ -20,25 +17,15 @@ def turn(board)
   else
     turn(board)
   end
-
 end
-#board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-#display_board(board)
-
-#puts "enter your number here:"
-
-#input=gets.strip
-input = 3
-
 
 def input_to_index(input)
+  #converts the user input into a useable index for the playing board.
   input.to_i - 1
 end
 
-index = input_to_index(input)
-
 def move(board, index, token = "X")
+  #assigns a token "X" to the specified index position on the playing board.
   board[index] = token
 end
 
@@ -52,8 +39,3 @@ def valid_move?(board, number)
   # returns 'true' if the inputed number is between 0 and 8, AND ALSO if the position is free as per the position_free? method.
   number.between?(0,8) && position_free?(board, number)
 end
-
-
-#move(board, index)
-#display_board(board)
-#puts valid_move?(board, index)
