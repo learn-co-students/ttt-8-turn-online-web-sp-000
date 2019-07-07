e7aa5a3fc60ea521a149b1d3fd8d0ebc6687a31e
chandra mohan <mohaniyer_78@hotmail.com>
2019-07-07 12:00:25 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,3 +37,24 @@ def move(board,index,value= "X")
   board[index] = value
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.chomp 
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+
+
+
+
+
+
+
+
+
