141a8cf368ad9e64014da37c35668482d15e5f3f
Aoune Syed <aoune007@gmail.com>
2019-08-21 04:14:25 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,35 @@
-  display_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+def display_board(board)
+separator = "|"
+lines = "-----------"
+
+puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+puts "#{lines}"
+puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+puts "#{lines}"
+puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+end
 
 def input_to_index(user_input)
-  user_input.to_i - 1
+  user_input.to_i -1
 end
 
 def move(board, index, current_player = "X")
   board[index] = current_player
 end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) || board[index] == nil
+        return false
+  else
+    return true
+  end
+end
+
+#def position_taken?(board, index)
+#  if board[index] == " " || board[index] == "" || board[index] == nil
+#      return true
+#    else
+#      return false
+#    end
+#  end
