b57e59e5e31e3d1b24df7ab7a5fef1d9e6f43ace
Ronda D Cowen <cowenhome5@gmail.com>
2019-09-20 21:12:10 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,3 +28,15 @@ def valid_move?(board, index)
     true
   end 
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+    user_input = gets.strip
+ index = input_to_index(user_input) 
+  if  valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else 
+    turn(board)
+  end
+end
