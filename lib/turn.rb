801696273f26f1ba435a6fbea22173864690deb2
Erin Stansbury <ekstansbury@gmail.com>
2019-12-12 03:53:04 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,11 +6,7 @@ puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  input_to_index(input)
-end
+
 
 def input_to_index(input)
   input.to_i - 1
