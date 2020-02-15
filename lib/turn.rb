c119319a07a2dc727483379e8fdfd0616a597389
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-15 23:44:28 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,10 +10,10 @@ end
 
 
 def valid_move?(board, index)
-if board[index] == " " 
-    return true
-  else 
-     return false 
+ if board[index] == " " &&  index.between?(0-8)
+     return true 
+   else 
+     return false
 end
 
 end
