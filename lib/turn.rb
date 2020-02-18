3aa8c2c8f57a943b5afaf4821de8db5f726d1b23
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-18 04:30:13 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,35 +7,35 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def turn
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  if user_input.to_i.between?(1, 9)
-    puts "value of user_input valid. #{user_input}" 
-    return user_input
-  else
-    puts "Incorrect Choice. #{user_input}"
-    turn 
-  end 
-end
-
-# validate user's choice is between 1 and 9
-def valid_move(board, index)
-  if board[index] == "X" || board[index] == "O"
-    return "false" 
-  else
-    return "true" 
-  end
-end 
-
-#converts user_input to interger then -1 then return as index
-def input_to_index(user_input)
-  user_input = user_input.to_i
-  index = user_input - 1
-end
-
 # collect moves to pass to the board
 def move(board, index, value = "X")
   board[index] = value 
 end
 
+def input_to_index(user_input)
+  index = user_input.to_i - 1 
+end
+
+def valid_move?(board, index)
+  if board[index] == " " || board[index] == ""
+      true 
+  elsif
+      board[index] == "X" || board[index] == "O"
+      false 
+  else
+      false
+  end 
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+    if  user_input.to_i.between?(1, 9)
+      index = input_to_index(user_input)
+      valid_move?(board, index)
+      move(board, index, value = "X")
+      display_board(board)
+   else
+      turn(board)
+  end 
+end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,6 +1,6 @@
-require_relative "../lib/turn.rb"
+require_relative '../lib/turn.rb'
 
-describe './lib/turn.rb' do
+describe '../lib/turn.rb' do
 
   describe '#display_board' do
     it 'prints arbitrary arrangements of the board' do
