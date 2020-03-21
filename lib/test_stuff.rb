60080fc12ad63b589cb7f290ec45483a851a273c
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-21 15:58:05 +0000

    Done.

--- lib/test_stuff.rb
+++ lib/test_stuff.rb
@@ -1,3 +1,5 @@
+board = [" ", " ", " ", "X", " ", "O", " ", " ", "X"]
+
 def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
@@ -16,6 +18,8 @@ def position_taken?(board, index)
   end
 end
 
+puts position_taken?(board, 5)
+
 def valid_move?(board, index)
   if index.between?(0, 8) && position_taken?(board, index) == false
     true
@@ -26,13 +30,16 @@ def valid_move?(board, index)
   end
 end
 
+puts valid_move?(board, 9)
+puts valid_move?(board, 8)
+puts valid_move?(board, 7)
+
 def move(board, index, token = "X")
   board[index] = "#{token}"
 end
 
-def input_to_index(input)
-  "#{input}".to_i - 1
-end
+ move(board, 2, "O")
+ puts display_board(board)
 
 def turn(board)
   # ask for input
@@ -40,7 +47,7 @@ def turn(board)
   #get input
   #convert input to index
   input = gets.strip
-  index = input_to_index(input)
+  index = "#{input}".to_i - 1
   #if index is valid
     #make the move for index
     #show the board
@@ -58,3 +65,5 @@ def turn(board)
     display_board(board)
     end
 end
+
+turn(board)
