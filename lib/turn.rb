8d9216669644d191d48cac1f24c5186e7cf166c0
strinie <hello@katherinesalamat.com>
2020-05-26 15:55:39 -0400

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,55 @@
+# Should accept a board as an argument and print out the current state of the board for the user.
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+  
+def valid_move?(board, index)
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+      return false
+    end
+end
+
+def position_taken?(array, index)
+  if array[index] == " " || array[index] == nil
+   return false
+  else
+   return true
+  end
+end
+  
+def on_board?(number)
+  if number.between?(0, 8) == true
+    return true
+  else
+    return false
+  end
+end
+  
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
+  
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
+end
+
+def turn(board)
+    puts "Please enter 1-9:"
+    number = gets.chomp
+    index = input_to_index(number)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+      turn(board)
+  end
+end
