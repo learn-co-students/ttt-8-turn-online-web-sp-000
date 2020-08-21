1bcbad6e2679d51fdf2b4c5ef2cb19450ec83bf7
chic-creator-9382 <danessypinedav@gmail.com>
2020-08-21 18:30:17 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,5 @@
 
 
-
-
-
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -12,15 +9,38 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-  if position_taken?(board, index) != true && index.between?(0,8)
-    true
-  else
-    false
+def input_to_index(user_input)
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+  return new_user_input
+end
+def move(board,index,character = "X")
+   board[index] = character
+  return board 
   end
+
+  def valid_move? (board,index)
+   position_taken?(board, index) != true && index.between?(0,8)
+end
 def position_taken?(board, index)
 if ( board[index] ==  " ") || (board[index] == "" ) || (board[index] == nil )
   return false
 else ( board[index] ==  "X") || (board[index] == "O" )
   return true 
-end
 end 
+end 
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+input = gets.strip
+index = input_to_index(input)
+if valid_move?(board, index)
+move(board, index, character = "X")
+else
+  input = gets.strip
+  index = input_to_index(input)
+  valid_move?(board, index)
+end
+display_board(board)
+end
