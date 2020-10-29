89c8c91046864637e714db73e5c6b6a8db659fa2
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-29 00:30:36 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,40 +1,43 @@
- 
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil 
-    return false 
-  elsif board[index] = "X" || board[index] = "O"
-    return false 
- end
-end   
-
-
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 def valid_move?(board, index)
-  if index.between?(0, 8)
-    then return true 
-     else puts "This is an invalid move"
-     end 
-   if board[index] = " " || board[index] = nil
-      return true
-      elsif board[index] = "X" || "O"
-        return false
-        puts "That position is already taken" 
-   end  
-end  
+  index = 0  
+  if index = " " || index = "" || index = nil 
+    return true
+  end 
+   if board[index] = "X" || board[index] = "O" 
+    return false 
+    turn(board) 
+  end  
+  if not index.between?(0,8)
+    return false     
+    turn(board)
+   end
+end 
 
 
 
+   
+def position_taken?(index) 
+   if board[index] = " " || board[index] = "" || board[index] = nil 
+     return false  
+  end 
+  if board[index] = "X" || board[index] = "O"
+    return true 
+  end 
+end 
+ 
+ 
 
-turn(board) 
-
-
-def display_board(board)
+ def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-  
   end
+  
+  
+  
   
 def input_to_index(input)
   if (1..9).cover?(input)
@@ -55,5 +58,10 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.chomp  
-end  
+  input_to_index(input)
+  valid_move?(board, input) 
+  position_taken?(board, index)
+  move(board, index, value)
+  display_board(board)
+end   
  
