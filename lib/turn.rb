f322fc3a22607a53a555cf046a015928e1282438
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-29 03:57:11 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,8 @@ def position_taken?(board, index)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "   
-  puts "-----------"  
+  puts "-----------"  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  
    end
    
   
