7ea07ec8479131770cba915d106131c7515432e3
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-29 02:35:50 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,37 +1,26 @@
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 def valid_move?(board, index)
-  index = 0  
-  # if index = " " || index = "" || index = nil
-  #   return true
-  # end 
-  # if board[index] = "X" || board[index] = "O" 
-  #   return false 
-  #   turn(board) 
-  # end  
-  if index.between?(0,8)
-    position_taken?(index) 
+   if index.between?(0,8)
+    position_taken?(index)
    end
 end 
 
 
 
    
-def position_taken?(index) 
-  # if board[index] = " " || board[index] = "" || #board[index] = nil 
-  #   return false  
-   
-  if board[index] = "X" || board[index] = "O"
-    return true 
+def position_taken?(board, index) 
+    if board[index] = "X" || board[index] = "O"
+    turn(board)
   end 
- end 
+ end  
  
-
+ 
  def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "-----------" 
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "  
   end
    
   
