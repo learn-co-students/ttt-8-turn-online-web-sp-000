3f579421598c028420e3c1abdfe87c96c561414f
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-30 18:50:50 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,19 +1,25 @@
+def turn(board)
+  puts "Please enter 1-9" 
+  input = gets.chomp  
+  input_to_index(input)
+ end   
+
+
 def valid_move?(board, number)
-#     if index.between?(0,8) && !position_taken?
-#     return true 
-#     end 
-#     position_taken(board, index)
+    if index.between?(0,8) && !position_taken 
+    position_taken 
+  else  turn(board)
  end
-
-
-
-def position_taken?(board, index) 
-  if board_index = "X" || board_index = "O"
-    return board(index)
-  end 
-     turn(board)
  end 
- 
+
+
+
+def position_taken?(board, number) 
+  if board_number = "X" || board_number = "O"
+     turn(board)
+   else display_board(board)
+  end 
+ end 
  
  
  def display_board(board)
@@ -28,26 +34,27 @@ end
   
   
 def input_to_index(input) 
-  if (1..9).cover?(input)
-    puts "invalid" 
-  else string = input.to_i 
-    index = string - 1 
-  end 
-end   
+  if (1..9) cover?(input)
+then index = string - 1 
+  else 
+    puts "invalid"
+    turn 
+    
+#   if (1..9).cover?(input)
+#     puts "invalid" 
+#   else string = number.to_i 
+#     index = string - 1 
+#   end 
+# end   
  
 
 def move(board, index, value = "X")
-  # board[index] = value 
-  # display_board(board)
+  board[index] = value 
+  display_board(board)
 end 
 
 
-def turn(board)
-  # puts "Please enter 1-9" 
-  # input = gets.chomp  
-  # number = input_to_index(input)
-  # valid_move?(board, index) 
- end   
+
  
  
  #adam weissman 
