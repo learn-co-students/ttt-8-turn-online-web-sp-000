e69b1f581e7a664da035b4aa46f1bff41ffc07ad
Lj <tnedutsl457892@gmail.com>
2021-02-10 07:14:29 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,29 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(array, index)
+  if (index < 0 || index > 8)
+    return false
+  elsif position_taken?(array, index)
+    return false
+  else
+    return true
+  end
+end
+
+def position_taken?(board, index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    return false
+  else
+    return true
+  end
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
