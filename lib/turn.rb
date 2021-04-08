741a5811ed1cb8e9b2614c6cb993707ac18bbcd6
Heather Wilson <hdaniwilson@gmail.com>
2021-04-08 21:33:30 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -45,7 +45,7 @@ def turn(board)
   user_input= gets.strip
     index= input_to_index(user_input)
     if valid_move?(board, index)
-      puts move(board, user_input, "X")
+      move(board, index, "X")
       display_board(board)
     else
       turn(board)
