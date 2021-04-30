68b0690226c68fbceb8fd4aa32ac9e8c50e844cd
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-04-30 20:31:49 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,52 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end 
+
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end 
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, player = "X")
+    display_board(board)
+  else 
+    turn(board)
+ end  
+end
+def input_to_index(user_input)
+  user_input.to_i - 1 
+end 
+
+def move(board, index, player = "X")
+  board[index] = player
+end 
+
+def position_taken? (board, index)
+  if board[index] == (" ") || board[index] == ("") || board[index] == nil 
+    return false 
+  else 
+    return true
+ end    
+end
+
+def valid_move? (board, index)
+  if !position_taken?(board, index) && (index).between?(0, 8)
+    return true
+  else 
+    return false
+  end
+ end
+
