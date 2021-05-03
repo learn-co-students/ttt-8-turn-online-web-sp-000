5e4086d02db0e02f13092737e19f09d0f6583c63
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-03 21:37:12 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,10 +28,14 @@ def valid_move?(board, index)
    end
 end
 
- def position_taken?(board, index)
-       if (board[index] == "  ") || (board[index] == " ") || (board[index] == nil)
-          return false 
-       else (board[index] == "X") || (board[index] == "O")
-          return true
-       end
+def position_taken?(board, index)
+  if (board[index] == "  ") || (board[index] == " ") || (board[index] == nil)
+    return false 
+  else (board[index] == "X") || (board[index] == "O")
+    return true
   end
+end
+  
+def turn(board)
+  puts "Please enter 1-9:"
+end
