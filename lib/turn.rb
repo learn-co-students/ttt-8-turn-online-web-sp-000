7c22d79df67bf0ebc9992853939998b71d6ad9a3
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-03 22:40:09 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,4 +38,12 @@ end
   
 def turn(board)
   puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+    if valid_move?(board, index)
+      move(board, index)
+    else 
+      turn(board)
+    end
+  display_board(board)
 end
