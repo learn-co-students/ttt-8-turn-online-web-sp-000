7e92471348eb18c819f088722b26f50bc3eba7cc
Chris Suarez <chrissuarez001@gmail.com>
2021-05-31 15:52:41 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,53 @@
+def display_board(board)
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def move(board ,index, current_player = "X")
+  board[index] = current_player
+end
+
+
+def input_to_index(user_input)
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+  return new_user_input
+end
+
+def position_taken? (board , index)
+  if board[index] == " " || board[index] == "" || board[index] == "nil"
+    return false
+  else
+    return true
+  end
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+
+def play(board)
+i = 0
+while i < 9
+  i += 1
+  turn(board)
+end
+end
